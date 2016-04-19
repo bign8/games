b087a84616a62ab305614781a87d5869e6c1d68c
Nate Woods <big.nate.w@gmail.com>
2016-04-19 13:20:15 -0600

    Reducing String() to 12ms generations

--- chess.go
+++ chess.go
@@ -1,9 +1,6 @@
 package chess
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
 // State is an internal representation of a chess game.
 // - FEN notation: https://en.wikipedia.org/wiki/FEN
@@ -19,15 +16,16 @@ type State struct {
 	count     uint32 // max of 4294967295 (limited by type)
 }
 
-var chrLookup = map[uint8]rune{
-	'p': '♟', 'r': '♜', 'n': '♞', 'b': '♝', 'q': '♛', 'k': '♚',
-	'P': '♙', 'R': '♖', 'N': '♘', 'B': '♗', 'Q': '♕', 'K': '♔',
+var chrLookup = map[uint8]string{
+	'p': "♟", 'r': "♜", 'n': "♞", 'b': "♝", 'q': "♛", 'k': "♚",
+	'P': "♙", 'R': "♖", 'N': "♘", 'B': "♗", 'Q': "♕", 'K': "♔",
 }
 
 var numLookup = map[uint8]int{
-	'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
+	'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7,
 }
 
+const col = " ║ "
 const top = "╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n"
 const sep = "\n╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
 const bot = "\n╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝\n  A   B   C   D   E   F   G   H"
@@ -46,39 +44,30 @@ func New() *State {
 
 // String is to implement the fmt.Stringer interface
 func (s State) String() string {
-	// top of board
-	out := ""
-	rows := make([]string, 8)
-
-	// Parse the game board
-	for i, cursor, scanner := 0, 0, 0; i < 64; i++ {
-
-		// Print Player Character
-		out += "║ "
-		if cursor == i {
-			in := s.board[scanner]
-			if chr, ok := chrLookup[in]; ok {
-				out += string(chr)
-				cursor++
-			} else {
-				out += " "
-				cursor += numLookup[in]
-			}
-			scanner++
-		} else {
-			out += " "
+	bits := make([]string, 64)
+	for i := 0; i < 64; i++ {
+		bits[i] = " "
+	}
+	for i, scanner := 0, 0; i < 64; i++ {
+		in := s.board[scanner]
+		scanner++
+		if chr, ok := chrLookup[in]; ok {
+			bits[i] = chr
+			continue
 		}
-
-		// Print horizontal row separator
-		if i%8 == 7 {
-			out += fmt.Sprintf(" ║  %d", 8-i/8) // TODO: fix this
-			rows[i/8] = out
-			out = ""
-		} else {
-			out += " "
-		}
+		i += numLookup[in]
 	}
 
+	rows := []string{
+		"║ " + strings.Join(bits[0:8], col) + " ║  8",
+		"║ " + strings.Join(bits[8:16], col) + " ║  7",
+		"║ " + strings.Join(bits[16:24], col) + " ║  6",
+		"║ " + strings.Join(bits[24:32], col) + " ║  5",
+		"║ " + strings.Join(bits[32:40], col) + " ║  4",
+		"║ " + strings.Join(bits[40:48], col) + " ║  3",
+		"║ " + strings.Join(bits[48:56], col) + " ║  2",
+		"║ " + strings.Join(bits[56:64], col) + " ║  1",
+	}
 	return top + strings.Join(rows, sep) + bot
 }
 
--- chess_test.go
+++ chess_test.go
@@ -1,6 +1,13 @@
 package chess
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"testing"
+	"unicode/utf8"
+)
 
 func BenchmarkNewString(b *testing.B) {
 	game := New()
@@ -11,25 +18,50 @@ func BenchmarkNewString(b *testing.B) {
 
 func TestNewString(b *testing.T) {
 	x := New().String()
-	y := "╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n"
-	y += "║ ♜ ║ ♞ ║ ♝ ║ ♛ ║ ♚ ║ ♝ ║ ♞ ║ ♜ ║  8\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║  7\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║   ║   ║   ║   ║   ║   ║   ║   ║  6\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║   ║   ║   ║   ║   ║   ║   ║   ║  5\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║   ║   ║   ║   ║   ║   ║   ║   ║  4\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║   ║   ║   ║   ║   ║   ║   ║   ║  3\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║  2\n"
-	y += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n"
-	y += "║ ♖ ║ ♘ ║ ♗ ║ ♕ ║ ♔ ║ ♗ ║ ♘ ║ ♖ ║  1\n"
-	y += "╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝\n"
-	y += "  A   B   C   D   E   F   G   H"
-	if x != y {
-		b.Errorf("String Missmatch\n%q\n%q", x, y)
+	lines := []string{
+		"╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗",
+		"║ ♜ ║ ♞ ║ ♝ ║ ♛ ║ ♚ ║ ♝ ║ ♞ ║ ♜ ║  8",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║ ♟ ║  7",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║   ║   ║   ║   ║   ║   ║   ║   ║  6",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║   ║   ║   ║   ║   ║   ║   ║   ║  5",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║   ║   ║   ║   ║   ║   ║   ║   ║  4",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║   ║   ║   ║   ║   ║   ║   ║   ║  3",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║ ♙ ║  2",
+		"╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+		"║ ♖ ║ ♘ ║ ♗ ║ ♕ ║ ♔ ║ ♗ ║ ♘ ║ ♖ ║  1",
+		"╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝",
+		"  A   B   C   D   E   F   G   H",
 	}
+	y := strings.Join(lines, "\n")
+	if x != y {
+		b.Errorf("String Missmatch\n%s", compare(x, y))
+	}
+}
+
+func compare(a, b string) string {
+	al := strings.Split(a, "\n")
+	bl := strings.Split(b, "\n")
+
+	max := 0
+	for i := 0; i < len(al); i++ {
+		if len(al[i]) > max {
+			max = utf8.RuneCountInString(al[i])
+		}
+	}
+	mask := fmt.Sprintf("%%-%ds", max+10)
+	// panic(mask)
+	log.Printf("Mash: %q", mask)
+
+	out := make([]string, len(al))
+	for i := 0; i < len(al); i++ {
+		out[i] = fmt.Sprintf(mask, al[i]) + bl[i]
+	}
+
+	return strings.Join(out, "\n") + " " + strconv.Itoa(max) + " " + mask
 }
