719892574eae68c109e8f1185476bcb4b28a0a01
Nate Woods <big.nate.w@gmail.com>
2016-05-11 00:39:32 -0600

    Adding logging + Benchmarks

---
+++ impl/ttt/ttt_test.go
@@ -0,0 +1,48 @@
+package ttt
+
+import (
+	"testing"
+
+	"github.com/bign8/games"
+	"github.com/bign8/games/player/cli"
+)
+
+func BenchmarkStateString(b *testing.B) {
+	game := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		game.String()
+	}
+}
+
+func BenchmarkNewState(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		New()
+	}
+}
+
+func BenchmarkApply(b *testing.B) {
+	p := cli.New("asdf", games.MinPlayer)
+	game := New(p, p)
+	move := game.Actions()[0]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		game.Apply(move)
+	}
+}
+
+func BenchmarkTerminal(b *testing.B) {
+	game := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		game.Terminal()
+	}
+}
+
+func BenchmarkUtility(b *testing.B) {
+	game := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		game.Utility()
+	}
+}
--- player/minimax/minimax.go
+++ player/minimax/minimax.go
@@ -1,10 +1,16 @@
 package minimax
 
-import "github.com/bign8/games"
+import (
+	"fmt"
+	"time"
+
+	"github.com/bign8/games"
+)
 
 type minimaxPlayer struct {
 	name  string
 	pType games.PlayerType
+	ctr   uint
 }
 
 // New creates a new player that interfaces with a human via Stdin/out/err
@@ -27,14 +33,18 @@ func (mm minimaxPlayer) Type() games.PlayerType {
 	return mm.pType
 }
 
-func (mm minimaxPlayer) Play(s games.State) games.Action {
-	a, _ := search(s)
+func (mm *minimaxPlayer) Play(s games.State) games.Action {
+	mm.ctr = 0
+	start := time.Now()
+	a, _ := mm.search(s)
+	fmt.Printf("%s explored %d games in %s\n", mm.name, mm.ctr, time.Since(start))
 	return a
 }
 
 // MiniMax searches the full game tree until terminal nodes
-func search(s games.State) (games.Action, int) {
+func (mm *minimaxPlayer) search(s games.State) (games.Action, int) {
 	if s.Terminal() {
+		mm.ctr++
 		// fmt.Printf("%s - %d\n", s, s.Utility())
 		return nil, s.Utility()
 	}
@@ -45,9 +55,9 @@ func search(s games.State) (games.Action, int) {
 
 	actions := s.Actions()
 	best := actions[0]
-	_, cap := search(s.Apply(best))
+	_, cap := mm.search(s.Apply(best))
 	for _, a := range actions[1:] {
-		if _, value := search(s.Apply(a)); compare(cap, value) {
+		if _, value := mm.search(s.Apply(a)); compare(cap, value) {
 			best, cap = a, value
 		}
 	}
