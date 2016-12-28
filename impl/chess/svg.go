da2cd8ae2e11c52cbcb16026b9f803244a7b2aa7
Nate Woods <big.nate.w@gmail.com>
2016-12-28 00:53:57 -0700

    Changing chess board colors

--- impl/chess/svg.go
+++ impl/chess/svg.go
@@ -6,8 +6,7 @@ import (
 )
 
 func (s State) SVG(bool) string {
-	whites := make([]string, 0, 16)
-	blacks := make([]string, 0, 16)
+	pieces := make([]string, 0, 32)
 	for i, chr := range s.toGrid() {
 		if chr == ' ' {
 			continue
@@ -15,13 +14,8 @@ func (s State) SVG(bool) string {
 		row := i % 8
 		col := i / 8
 		str := fmt.Sprintf(`<text x="%d" y="%d.75">%c</text>`, row, col, chr)
-		if row%2 == 0 && col%2 == 0 || row%2 == 1 && col%2 == 1 {
-			blacks = append(blacks, str)
-		} else {
-			whites = append(whites, str)
-		}
+		pieces = append(pieces, str)
 	}
-	black := `<g style="font-size:1px">` + strings.Join(blacks, "") + "</g>"
-	white := `<g style="font-size:1px;fill:white">` + strings.Join(whites, "") + "</g>"
-	return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-.05 -.05 8.1 8.1">` + black + white + `</svg>`
+	parts := `<g style="font-size:1px">` + strings.Join(pieces, "") + "</g>"
+	return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-.05 -.05 8.1 8.1">` + parts + `</svg>`
 }
