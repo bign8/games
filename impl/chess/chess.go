d7f7777b199fd7b0e7671a41624ac42a819097ad
Nate Woods <big.nate.w@gmail.com>
2016-12-27 22:05:21 -0700

    Cleaning up string implementation

--- impl/chess/chess.go
+++ impl/chess/chess.go
@@ -163,14 +163,9 @@ func (s State) Terminal() bool {
 }
 
 var Game = games.Game{
-	Name: "Chess",
-	Slug: "chess",
-	Board: `
-<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" width="810"
-height="810" viewBox="-.05 -.05 8.1 8.1">
-<rect x="-.5" y="-.5" width="9" height="9"/>
-<path fill="#FFF" d="M0,0H8v1H0zm0,2H8v1H0zm0 2H8v1H0zm0,2H8v1H0zM1,0V8h1V0zm2,0V8h1V0zm2 0V8h1V0zm2,0V8h1V0z"/></svg>`,
+	Name:    "Chess",
+	Slug:    "chess",
+	Board:   `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-.05 -.05 8.1 8.1"><rect x="-.5" y="-.5" width="9" height="9"/><path fill="#FFF" d="M0,0H8v1H0zm0,2H8v1H0zm0 2H8v1H0zm0,2H8v1H0zM1,0V8h1V0zm2,0V8h1V0zm2 0V8h1V0zm2,0V8h1V0z"/></svg>`,
 	Players: []string{"White", "Black"},
 	Start:   New,
 	AI:      minimax.New("0"),
--- impl/chess/svg.go
+++ impl/chess/svg.go
@@ -1,5 +1,10 @@
 package chess
 
+import "strings"
+
 func (s State) SVG(bool) string {
-	return "<!-- TODO -->"
+	top := `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-.05 -.05 8.1 8.1"><g style="font-size:1px">`
+	pieces := make([]string, 0, 32)
+	pieces = append(pieces, `<text x="7" y="7.75">♟</text>`)
+	return top + strings.Join(pieces, "") + `</g></svg>`
 }
