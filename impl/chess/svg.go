31bb54ea7b4498afb999c4ea93e79ccb3a08a18d
Nate Woods <big.nate.w@gmail.com>
2016-12-27 22:25:20 -0700

    Actually getting a chess SVG representation

--- impl/chess/svg.go
+++ impl/chess/svg.go
@@ -1,10 +1,27 @@
 package chess
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func (s State) SVG(bool) string {
-	top := `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-.05 -.05 8.1 8.1"><g style="font-size:1px">`
-	pieces := make([]string, 0, 32)
-	pieces = append(pieces, `<text x="7" y="7.75">♟</text>`)
-	return top + strings.Join(pieces, "") + `</g></svg>`
+	whites := make([]string, 0, 16)
+	blacks := make([]string, 0, 16)
+	for i, chr := range s.toGrid() {
+		if chr == ' ' {
+			continue
+		}
+		row := i % 8
+		col := i / 8
+		str := fmt.Sprintf(`<text x="%d" y="%d.75">%c</text>`, row, col, chr)
+		if row%2 == 0 && col%2 == 0 || row%2 == 1 && col%2 == 1 {
+			blacks = append(blacks, str)
+		} else {
+			whites = append(whites, str)
+		}
+	}
+	black := `<g style="font-size:1px">` + strings.Join(blacks, "") + "</g>"
+	white := `<g style="font-size:1px;fill:white">` + strings.Join(whites, "") + "</g>"
+	return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-.05 -.05 8.1 8.1">` + black + white + `</svg>`
 }
