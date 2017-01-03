47a6aa57af686ebb6b99dc23ebcb2d1ad37ee959
Nate Woods <big.nate.w@gmail.com>
2017-01-02 22:47:30 -0700

    Breaking into real html templates

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"golang.org/x/net/websocket"
 
 	"github.com/bign8/games/impl"
+)
+
+var (
+	p       = func(n string) string { return filepath.Join("cmd", "server", "tpl", n+".gohtml") }
+	rootTpl = template.Must(template.ParseFiles(p("base"), p("root")))
+	gameTpl = template.Must(template.ParseFiles(p("base"), p("game")))
+	infoTpl = template.Must(template.ParseFiles(p("base"), p("info")))
 )
 
 func main() {
@@ -34,4 +43,35 @@ func main() {
 func randomHandler(w http.ResponseWriter, r *http.Request) {
 	urlStr := fmt.Sprintf("/play/%s", impl.Rand())
 	http.Redirect(w, r, urlStr, http.StatusTemporaryRedirect)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	rootTpl.Execute(w, struct {
+		Games interface{}
+	}{
+		Games: impl.Map(),
+	})
+}
+
+func gameHandler(w http.ResponseWriter, r *http.Request) {
+	game, ok := impl.Get(mux.Vars(r)["slug"])
+	if !ok {
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	gameTpl.Execute(w, struct {
+		Game  interface{}
+		Board template.HTML
+	}{
+		Game:  game,
+		Board: template.HTML(game.Board),
+	})
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	infoTpl.Execute(w, nil)
 }
