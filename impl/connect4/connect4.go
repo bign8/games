8bd6f1e4d2df57ad66cfa1198bcfe9984c19db64
Nate Woods <big.nate.w@gmail.com>
2017-01-04 22:06:58 -0700

    Stubbing out more connect4

--- impl/connect4/connect4.go
+++ impl/connect4/connect4.go
@@ -1,6 +1,7 @@
 package connect4
 
 import (
+	"errors"
 	"strconv"
 
 	"github.com/bign8/games"
@@ -11,7 +12,7 @@ var (
 	_    games.Action = (*c4move)(nil)
 	Game              = games.Game{
 		Name: "Connect 4",
-		Slug: "connect4",
+		Slug: "c4",
 		// TODO: https://upload.wikimedia.org/wikipedia/commons/d/dc/Puissance4_01.svg
 		Board: `<svg width="350" height="300" viewBox="1 1 7 6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 		<defs>
@@ -70,10 +71,18 @@ var (
 		</svg>
 `,
 		Players: []string{"Red", "Yellow"},
-		Start:   nil,
+		Start:   New,
 		AI:      nil,
 	}
 )
+
+func New(actors ...games.Actor) games.State {
+	// FUTURE: can we assume this?
+	if len(actors) != 2 {
+		return &c4{err: errors.New("Invalid number of players")}
+	}
+	return &c4{players: actors}
+}
 
 type c4 struct {
 	board   [7][]byte
@@ -82,9 +91,51 @@ type c4 struct {
 	err     error
 }
 
-func (s *c4) String() string                 { return "TODO" }
-func (s *c4) Player() games.Actor            { return nil }
-func (s *c4) Apply(games.Action) games.State { return nil }
+func (s *c4) Player() games.Actor { return s.players[s.ctr%2] }
+
+func (s *c4) String() string {
+	out := "+-+-+-+-+-+-+-+\n"
+	for i := 5; i >= 0; i-- {
+		out += "|"
+		for j := 0; j < 7; j++ {
+			if len(s.board[j]) > i {
+				switch s.board[j][i] {
+				case 'R':
+					out += "R"
+				case 'Y':
+					out += "Y"
+				default:
+					out += " "
+				}
+			} else {
+				out += " "
+			}
+			out += "|"
+		}
+		out += "\n"
+		if i != 0 {
+			out += "+-+-+-+-+-+-+-+\n"
+		}
+	}
+	return out + "+-+-+-+-+-+-+-+"
+}
+
+func (s *c4) Apply(action games.Action) games.State {
+	a, ok := action.(c4move)
+	if !ok {
+		return &c4{err: errors.New("Invalid Move")}
+	}
+	idx := int(uint8(a))
+	next := &c4{
+		ctr:     s.ctr + 1,
+		players: s.players,
+		err:     s.err,
+	}
+	copy(next.board[:], s.board[:])
+	next.board[idx] = append(next.board[idx], s.Player().Name()[0])
+	return next
+}
+
 func (s *c4) Actions() []games.Action {
 	acts := make([]games.Action, 0, 7)
 	for i := 0; i < 7; i++ {
@@ -94,11 +145,12 @@ func (s *c4) Actions() []games.Action {
 	}
 	return acts
 }
-func (s *c4) Terminal() bool          { return true }
+func (s *c4) Terminal() bool          { return false }
 func (s *c4) Utility(games.Actor) int { return 0 }
 func (s *c4) Error() error            { return s.err }
 func (s *c4) SVG(bool) string         { return "" }
 
+// Which column to drop the players token
 type c4move uint8
 
 func (move c4move) String() string { return "Column " + strconv.Itoa(int(move)+1) }
