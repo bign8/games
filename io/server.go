285dc6751a49dc2d8ff6594200e482d0cf42940e
Nate Woods <big.nate.w@gmail.com>
2017-07-27 23:27:40 -0600

    Adding https + http/2 for pusher fun

--- io/server.go
+++ io/server.go
@@ -7,7 +7,9 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/websocket"
 )
@@ -20,14 +22,49 @@ func sock(ws *websocket.Conn) {
 	// TODO: http://www.meetspaceapp.com/2016/03/29/tutorial-getting-started-websockets-go.html
 }
 
+var htmlTest = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Testing...</title>
+		<link type="text/css" rel="stylesheet" href="/test/css.css">
+		<script type="text/javascript" src="/test/js.js"></script>
+	</head>
+	<body>
+
+	</body>
+</html>`
+
+func test(w http.ResponseWriter, r *http.Request) {
+	if strings.HasSuffix(r.URL.Path, ".html") {
+		log.Println(r.Proto)
+		if p, ok := w.(http.Pusher); ok {
+			log.Println("Promoted!")
+			p.Push("/test/css.css", nil) // err
+		}
+		w.Write([]byte(htmlTest)) // err
+	} else {
+		w.Write([]byte(`static`)) // err
+	}
+}
+
+func run(f func() error, c chan<- error) { c <- f() }
+
 // http://buildnewgames.com/real-time-multiplayer/
+// https://github.com/denji/golang-tls
 func main() {
 	// TODO: use http 2.0 to push more assets to the consumer
 	// TODO: use service workers on the client side as necessary
 	fmt.Println("Running server...")
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/test/", test)
 	http.Handle("/ws", websocket.Handler(sock))
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		panic("ListenAndServe: " + err.Error())
+
+	errc := make(chan error, 1)
+	go run(func() error { return http.ListenAndServe(":8000", nil) }, errc)
+	go run(func() error { return http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil) }, errc)
+	err := <-errc
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
